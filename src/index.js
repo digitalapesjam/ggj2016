22948ead9d57baf57c7eebe328d0439c36df2012
jurgob <jurgo.boemo@gmail.com>
2016-01-29 22:11:57 +0100

    loaded da phucking image

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ class Game extends Phaser.Game {
 		super(500, 500, Phaser.AUTO, 'content', null);
 		this.state.add(states.StartMenu, StartMenu, false);
 		this.state.add(states.GameState, GameState, false);
-    this.state.add(states.EnemyTestLevel, EnemyTestLevel, false);
-		this.state.start(states.EnemyTestLevel);
+    	// this.state.add(states.EnemyTestLevel, EnemyTestLevel, false);
+		// this.state.start(states.EnemyTestLevel);
+		this.state.add('PlayerExampleLevel', PlayerExampleLevel, false);
+		this.state.start('PlayerExampleLevel');
 	}
 
 }
