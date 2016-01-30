69904a4375a06caf8f477c0e20a9945308c212b0
Francesco Zanitti <fzanitti@gmail.com>
2016-01-30 22:56:07 +0100

    sensors and doors

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -6,6 +6,9 @@ class GameState extends Phaser.State {
   preload(){
     this.game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
     this.game.load.image('tiles-1', 'assets/tiles-1.png');
+    // this.game.load.image('min', 'assets/min.png');
+    this.game.load.image('sensor', 'assets/sensor.png');
+    this.game.load.spritesheet('door', 'assets/door.png', 16, 64);
     this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     this.game.load.spritesheet('droid', 'assets/droid.png', 32, 32);
     this.game.load.image('starSmall', 'assets/star.png');
@@ -22,10 +25,16 @@ class GameState extends Phaser.State {
 
     this.map = this.game.add.tilemap('level1');
     this.map.addTilesetImage('tiles-1');
+    // this.map.addTilesetImage('min');
     this.map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]);
 
     this.layer = this.map.createLayer('Tile Layer 1');
+    // this.layer2 = this.map.createLayer('Tile Layer 2');
 
+    // this.map.setTileIndexCallback([90], (sprite, tile) => {
+    //   console.log('hit', {sprite, tile});
+    // }, null, this.layer2);
+    //
     //Un-comment this on to see the collision tiles
     //this.layer.debug = true;
 
@@ -37,23 +46,76 @@ class GameState extends Phaser.State {
     this.gameObjects['player'] = new Player(this.game,32,32);
     this.gameObjects['zombie'] = new Zombie(this.game,100,40);
     this.game.camera.follow(this.gameObjects['player']);
+
+    const sensorsSpec = JSON.parse(`[${this.map.properties.sensors}]`);
+    const game = this.game;
+
+    this.sensors = [];
+    const sensors = this.sensors;
+    const g = this.game;
+    sensorsSpec.forEach(([[stx, sty], [dtx, dty]]) => {
+
+      const sensorSprite = game.add.sprite(stx * 16, sty * 16, 'sensor');
+      sensorSprite.anchor.y = 1;
+
+      const doorSprite = game.add.sprite(dtx * 16, dty * 16, 'door');
+      doorSprite.anchor.y = 1;
+      doorSprite.animations.add('open', [1]);
+      doorSprite.animations.add('close', [0]);
+      g.physics.arcade.enable(doorSprite);
+      doorSprite.body.allowGravity = false;
+      doorSprite.body.immovable = true;
+
+      doorSprite.play('close');
+      sensors.push({sensorSprite, doorSprite, hit: false});
+    });
+
 	}
 
   update(){
     const game = this.game;
     const that = this;
+
+    this.sensors.forEach(s => {
+      const hit = that.gameObjects.player.overlap(s.sensorSprite);
+      if (hit !== s.hit) {
+        console.log('hit it: ' + hit);
+        s.hit = hit;
+        if (hit) {
+          s.doorSprite.play('open');
+        }
+        else {
+          s.doorSprite.play('close');
+        }
+      }
+    });
+
+    const sensors = this.sensors;
     this.gameObjects.player.corpses.forEach((corpse) =>{ //corpses collisions
       game.physics.arcade.collide(corpse, that.layer);
       Object.keys(that.gameObjects).forEach((key) => {
           game.physics.arcade.collide(corpse, that.gameObjects[key]);
       });
+      sensors.forEach(sensor => {
+        if (!sensor.hit) {
+          const hit = corpse.overlap(sensor.sensorSprite);
+          if (hit) {
+            sensor.doorSprite.play('open');
+            sensor.hit = true;
+          }
+        }
+      });
     });
 
     Object.keys(this.gameObjects).forEach((key)=>{
       game.physics.arcade.collide(that.gameObjects[key], that.layer);
+      sensors.forEach(({doorSprite, hit}) => {
+        if (!hit) {
+          game.physics.arcade.collide(that.gameObjects[key], doorSprite);
+        }
+      });
       that.gameObjects[key].update(that);
     });
-
   }
 
 }
