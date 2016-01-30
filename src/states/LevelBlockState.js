5f7e8e24bc98f59c4270650711f2277ad768e41d
Francesco Zanitti <fzanitti@gmail.com>
2016-01-30 13:59:27 +0100

    added level configurator, mock assets, and test state

--- src/states/LevelBlockState.js
+++ src/states/LevelBlockState.js
@@ -1,12 +1,39 @@
 import {System, CollisionChecker} from '../util';
 import DoorAnimation from 'objects/animations/DoorAnimation';
 import PressureDoor from 'objects/PressureDoor';
+import {preload as levelBlockPreload, Level, Config, GROUND, JUMP, HIGH_JUMP} from 'objects/Level';
 
 class GameState extends Phaser.State {
 
   preload() {
     this.load.spritesheet(DoorAnimation.label, DoorAnimation.path, 70, 70);
     this.load.image('sensor', 'assets/sprites/sensor.png');
+    levelBlockPreload(this);
+  }
+
+  createLevel(hero) {
+    const wHgt = this.game.world.height;
+    const c = new Config(wHgt);
+    c
+    .add(GROUND)
+    .add(GROUND)
+    .add(JUMP)
+    .add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND)
+    .add(HIGH_JUMP).add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND)
+    .add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND)
+    .add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND)
+    .add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND).add(GROUND);
+
+    const doors = [
+      {
+        sensor: {x: 100, y: wHgt},
+        door: {x: 400, y: wHgt},
+      },
+    ];
+
+    console.log('making level');
+    const level = new Level('lvl1', this.game, c.terrain(), doors, this.collisions, hero);
+    this.ecsystem.register('level', level);
   }
 
 	create() {
@@ -19,24 +46,25 @@ class GameState extends Phaser.State {
     // const sensorSpr = this.game.add.sprite(center.x - 50, center.y, 'sensor');
     // // add targe sprite
     const someSpr = this.game.add.sprite(0, 0, DoorAnimation.label);
-    let deleteDoor = null;
+    this.createLevel(someSpr);
+    // let deleteDoor = null;
     this.ecsystem.register('hero', {
       update: () => {
         someSpr.x = game.input.activePointer.x;
         someSpr.y = game.input.activePointer.y;
-        if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-          deleteDoor();
-        }
+        // if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+        //
+        // }
       }
     });
-    // const doorAnim = new DoorAnimation(this.game, center.x + 50, center.y);
-    deleteDoor = PressureDoor(
-      game,
-      this.collisions,
-      someSpr,
-      {x: center.x - 50, y: center.y},
-      {x: center.x + 50, y: center.y}
-    );
+    // // const doorAnim = new DoorAnimation(this.game, center.x + 50, center.y);
+    // deleteDoor = PressureDoor(
+    //   game,
+    //   this.collisions,
+    //   someSpr,
+    //   {x: center.x - 50, y: center.y},
+    //   {x: center.x + 50, y: center.y}
+    // );
 	}
 
 	update() {
