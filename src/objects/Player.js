f78520417eb66afb31a9d344d52e6ad17e42cca6
Paolo Burelli <burelli@gmail.com>
2016-01-30 18:24:04 +0100

    the real folk blues

---
+++ src/objects/Player.js
@@ -0,0 +1,76 @@
+export default class Player extends Phaser.Sprite {
+  constructor(game,x,y,resource){
+    super(game,x,y,resource);
+
+    this.game = game;
+    game.add.existing(this);
+    game.physics.enable(this, Phaser.Physics.ARCADE);
+
+    this.body.bounce.y = 0.2;
+    this.body.collideWorldBounds = true;
+    this.body.setSize(20, 32, 5, 16);
+
+    this.animations.add('left', [0, 1, 2, 3], 10, true);
+    this.animations.add('turn', [4], 20, true);
+    this.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    this.cursors = game.input.keyboard.createCursorKeys();
+    this.jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    this.facing = 'left';
+    this.jumpTimer = 0;
+  }
+
+  update(){
+    this.body.velocity.x = 0;
+    const cursors = this.cursors;
+    const jumpButton = this.jumpButton;
+
+    if (cursors.left.isDown)
+        {
+            this.body.velocity.x = -150;
+
+            if (this.facing != 'left')
+            {
+                this.animations.play('left');
+                this.facing = 'left';
+            }
+        }
+        else if (cursors.right.isDown)
+        {
+            this.body.velocity.x = 150;
+
+            if (this.facing != 'right')
+            {
+                this.animations.play('right');
+                this.facing = 'right';
+            }
+        }
+        else
+        {
+            if (this.facing != 'idle')
+            {
+                this.animations.stop();
+
+                if (this.facing == 'left')
+                {
+                    this.frame = 0;
+                }
+                else
+                {
+                    this.frame = 5;
+                }
+
+                this.facing = 'idle';
+            }
+        }
+
+        if (jumpButton.isDown && this.body.onFloor() && this.game.time.now > this.jumpTimer)
+        {
+            this.body.velocity.y = -250;
+            this.jumpTimer = this.game.time.now + 750;
+        }
+
+
+  }
+}
--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,12 +1,45 @@
-import RainbowText from 'objects/RainbowText';
+import Player from 'objects/Player';
 
 class GameState extends Phaser.State {
 
+  preload(){
+    this.game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+    this.game.load.image('tiles-1', 'assets/tiles-1.png');
+    this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    this.game.load.spritesheet('droid', 'assets/droid.png', 32, 32);
+    this.game.load.image('starSmall', 'assets/star.png');
+    this.game.load.image('starBig', 'assets/star2.png');
+    this.game.load.image('background', 'assets/background2.png');
+  }
+
 	create() {
-		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-		text.anchor.set(0.5);
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.game.stage.backgroundColor = '#000000';
+    this.background = this.game.add.tileSprite(0, 0, 800, 600, 'background');
+    this.background.fixedToCamera = true;
+
+    this.map = this.game.add.tilemap('level1');
+    this.map.addTilesetImage('tiles-1');
+    this.map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]);
+
+    this.layer = this.map.createLayer('Tile Layer 1');
+
+    //Un-comment this on to see the collision tiles
+    //this.layer.debug = true;
+
+    this.layer.resizeWorld();
+
+    this.game.physics.arcade.gravity.y = 250;
+
+    this.player = new Player(this.game,32,32,'dude');
+    this.game.camera.follow(this.player);
 	}
+
+  update(){
+    this.game.physics.arcade.collide(this.player, this.layer);
+    this.player.update();
+
+  }
 
 }
 
