9c9ee3e6d6a20b3dd0b92a25cc9994d488e7bda5
jurgob <jurgo.boemo@gmail.com>
2016-01-31 01:34:45 +0100

    no idea about what I did

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -15,17 +15,11 @@ export default class Player extends Phaser.Sprite {
     this.body.collideWorldBounds = true;
     this.body.setSize(42, 50, 5, 16);
 
-
-    this.animations.add('turn', [4], 20, true);
-    this.animations.add('attack', [2,1,0], 10, false);
-    // this.animations.add('attack_right', [17,18,19], 10, false);
+    this.animations.add('attack', [17, 18, 19], 10, false);
     this.animations.add('seppucku', [18, 18, 35], 20, false);
-    // this.animations.add('right', [10, 11,12,13,14,15,16], 10, true);
-    // this.animations.add('left', [3,4,5,6,7,8,9], 10, true);
-    this.animations.add('walk', [3,4,5,6,7,8,9], 10, true);
-    this.animations.add('stop_left', [3], 5, true);
-    this.animations.add('stop_right', [16], 5, true);
-    this.animations.add('jump', [20], 5, true);
+    this.animations.add('walk', [10,11,12,13,14,15,16], 10, true);
+    this.animations.add('stop', [16], 5, true);
+    this.animations.add('jump', [20], 20, true);
 
 
 
@@ -36,15 +30,18 @@ export default class Player extends Phaser.Sprite {
 
     // console.log('(Phaser.Keyboard ', Object.keys(Phaser.Keyboard));
 
-    this.facing = 'left';
-    this.direction = 'left';
+    this.direction = 1;
     this.jumpTimer = 0;
     this.corpses = [];
+    this.isJumping = false;
+    this.animations.play('stop');
   }
 
   update(){
-    console.log('this.scale.x ', this.scale.x)
-
+    const almostGrounded = this.body.onFloor() || 0.2 > Math.abs(this.body.deltaY());
+    let canJump = almostGrounded && this.game.time.now > this.jumpTimer
+    if(this.isJumping)
+      console.log('this.isJumping !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
     this.body.velocity.x = 0;
     const cursors = this.cursors;
     const jumpButton = this.jumpButton;
@@ -52,7 +49,6 @@ export default class Player extends Phaser.Sprite {
     if(this.animations.currentAnim.name === 'seppucku' && this.animations.currentAnim.isPlaying ) {
       return ;
     }
-
     if(this.seppuckuButton.isDown && !!!this.justSeppukued ){
       console.log('seppucku!!')
       this.justSeppukued = true;
@@ -62,21 +58,15 @@ export default class Player extends Phaser.Sprite {
       }, 1000);
       this.body.velocity.x = 0
       this.animations.play('seppucku');
-
       const pos = this.body.position
       this.corpses.push(new PlayerCorpse(this.game,pos.x,pos.y-15,'dude'));
-
       this.x = this.initialPosition.x;
       this.y = this.initialPosition.y;
-
-
     }
-
 
     if(this.attackButton.isDown){
       this.body.velocity.x = 0
       this.body.velocity.y = 0
-
       this.animations.play('attack');
     }
     if(this.animations.currentAnim.name === 'attack' && this.animations.currentAnim.isPlaying){
@@ -85,58 +75,31 @@ export default class Player extends Phaser.Sprite {
     if(cursors.left.isDown || cursors.right.isDown) {
       var d = (cursors.right.isDown) ? 1 : -1;
       this.direction = d;
-      this.scale.x = -d
+      this.scale.x = d
       this.body.velocity.x = d*150;
-      this.animations.play('walk');
+
+      if(!this.isJumping)
+        this.animations.play('walk');
+    } else {
+      // if(!this.isJumping){
+        this.animations.stop();
+        // this.animations.play('stop');
+      // }
     }
 
-    // if (cursors.left.isDown){
-    //   this.body.velocity.x = -150;
-    //   this.direction = 1;
-    //   this.scale.x = this.direction;
-    //
-    //   if (this.facing != 'left')
-    //   {
-    //     this.animations.play('walk');
-    //     this.facing = 'left';
-    //   }
-    // }else if (cursors.right.isDown){
-    //   this.direction = -1;
-    //   this.body.velocity.x = 150;
-    //   this.scale.x = this.direction;
-    //   if (this.facing != 'right')
-    //   {
-    //     this.animations.play('walk');
-    //     this.facing = 'right';
-    //   }
-    // }
-    else
-    {
-      if (this.facing != 'idle')
-      {
-        // this.animations.stop();
 
-        if (this.facing == 'left')
-        {
-          this.animations.play('stop_left');
-          // this.frame = 0;
-        }
-        else
-        {
-          this.animations.play('stop_right');
-          // this.frame = 5;
-        }
 
-        this.facing = 'idle';
-      }
-    }
-
-    const almostGrounded = this.body.onFloor() || 0.2 > Math.abs(this.body.deltaY());
-    if (jumpButton.isDown && almostGrounded && this.game.time.now > this.jumpTimer)
+    if(canJump)
+      this.isJumping = false;
+    if (jumpButton.isDown && canJump)
     {
       this.body.velocity.y = -150;
       this.jumpTimer = this.game.time.now + 750;
-      // this.animations.play('jump');
+      this.isJumping = true;
+      // this.animations.stop();
+      this.animations.play('jump');
+      console.log('jump anim')
+
     }
   }
 }
