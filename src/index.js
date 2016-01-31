674c6fc323adafa33dec729509c77abee64f918a
jurgob <jurgo.boemo@gmail.com>
2016-01-31 12:53:09 +0100

    add game over state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import GameState from 'states/GameState';
+import GameOver from 'states/GameOver';
+
 
 class Game extends Phaser.Game {
 
 	constructor() {
 		super(800, 600, Phaser.AUTO, 'content', null);
 		this.state.add('GameState', GameState, false);
-		this.state.start('GameState');
+    this.state.add('GameOver', GameOver, false);
+    this.state.start('GameState');
+    // this.state.start('GameOver');
 	}
 
 }
